86c9fac1e086628394cea4933d422b0aaab5fd6d
rgbosque <rgbosque@gmail.com>
2017-08-19 15:51:23 +0800

    create login form

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin
 
@@ -7,17 +11,48 @@ from flask_login import LoginManager, UserMixin
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(app.root_path, 'pm.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.config['SECRETY_KEY'] = 'thisissecret'
+app.config['SECRET_KEY'] = 'thisissecret'
 
+Bootstrap(app)
 db = SQLAlchemy(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 
 
-@app.route("/")
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(80))
+
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = StringField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('remember me')
+
+
+@app.route('/')
 def index():
-    return "Hello"
+    return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+
+    return render_template('login.html', form=form)
+
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 
 
 if __name__ == "__main__":
