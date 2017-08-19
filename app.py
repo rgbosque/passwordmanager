42487f99c7745b04e4cc6661b9ce26fbb9a8f88f
rgbosque <rgbosque@gmail.com>
2017-08-19 17:05:43 +0800

    registration and login code implementation

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
@@ -50,7 +50,13 @@ def login():
     form = LoginForm()
 
     if form.validate_on_submit():
-        return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if user.password == form.password.data:
+                return redirect(url_for('dashboard'))
+
+        return '<h1> Invalid username or password </h1>'
+
     return render_template('login.html', form=form)
 
 
@@ -59,7 +65,13 @@ def signup():
     form = RegistrationForm()
 
     if form.validate_on_submit():
-        return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
+        new_user = User(username=form.username.data,
+                        email=form.email.data, password=form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return '<h1> New user has been created! </h1>'
+
     return render_template('signup.html', form=form)
 
 
